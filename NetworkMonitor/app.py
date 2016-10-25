d5bf01d3593d20b0791e2d0f75e6e64625078970
vagrant <vagrant@localhost.localdomain>
2016-10-25 23:42:18 +0000

    added ping function integration with db

--- NetworkMonitor/app.py
+++ NetworkMonitor/app.py
@@ -1,6 +1,7 @@
 #imports the render_template and request used later
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo	#pymongo, hooks us up to the mongodb
+from util import ping
 
 app = Flask(__name__)	#the dunder is a weird thing
 mongo = PyMongo(app)	#
@@ -24,5 +25,20 @@ def suggestions():
 	suggestions = [obj['suggestion'] for obj in mongo.db.suggestions.find()]
 
 	return render_template('suggestions.html',suggestions=suggestions)
+
+@app.route('/ping', methods=('GET', 'POST'))
+def pingForm():
+	if request.method == 'POST':
+		hostIP = request.form['hostIP']
+		rtt, jitter = ping(hostIP);
+		result = mongo.db.hostIPs.insert_one({'hostIP':	{
+						'ip': hostIP,
+						'rtt': rtt,
+						'jitter': jitter}
+							})
+
+	hostIPs = [obj['hostIP'] for obj in mongo.db.hostIPs.find()]
+
+	return render_template('ping.html', hostIPs=hostIPs)
 
 app.run(host='0.0.0.0', port=8000)
