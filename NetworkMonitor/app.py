520d674e1622df84f908ee251d8891b89cd83601
Eddie Bracho <eddieb@liatrio.com>
2016-10-27 22:53:56 +0000

    Error checking for ping()

--- NetworkMonitor/app.py
+++ NetworkMonitor/app.py
@@ -1,44 +1,45 @@
 #imports the render_template and request used later
 from flask import Flask, render_template, request
-from flask_pymongo import PyMongo	#pymongo, hooks us up to the mongodb
+from flask_pymongo import PyMongo    #pymongo, hooks us up to the mongodb
 from util import ping
 
-app = Flask(__name__)	#the dunder is a weird thing
-mongo = PyMongo(app)	#
+app = Flask(__name__)    #the dunder is a weird thing
+mongo = PyMongo(app)    #
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/<name>')
 def hello(name):
-	return render_template('name.html',name=name)
+    return render_template('name.html',name=name)
 
 items = ['hello', 'there', 'everyone']
 
 @app.route('/suggestions', methods=('GET', 'POST'))
 def suggestions():
-	if request.method == 'POST':
-		suggestion = request.form['suggestion']
-		mongo.db.suggestions.insert_one({'suggestion': suggestion})
-	
-	suggestions = [obj['suggestion'] for obj in mongo.db.suggestions.find()]
+    if request.method == 'POST':
+        suggestion = request.form['suggestion']
+        mongo.db.suggestions.insert_one({'suggestion': suggestion})
 
-	return render_template('suggestions.html',suggestions=suggestions)
+    suggestions = [obj['suggestion'] for obj in mongo.db.suggestions.find()]
+
+    return render_template('suggestions.html',suggestions=suggestions)
 
 @app.route('/ping', methods=('GET', 'POST'))
-def pingForm():
-	if request.method == 'POST':
-		hostIP = request.form['hostIP']
-		rtt, jitter = ping(hostIP);
-		result = mongo.db.hostIPs.insert_one({'hostIP':	{
-						'ip': hostIP,
-						'rtt': rtt,
-						'jitter': jitter}
-							})
+def ping_form():
+    error = None
+    if request.method == 'POST':
+        hostIP = request.form['hostIP']
+        try:
+            rtt, jitter = ping(hostIP);
+        except ValueError as e:
+            error = str(e)
+        else:
+            entry = {'hostIP': {'ip': hostIP,'rtt': rtt,'jitter': jitter}}
+            result = mongo.db.hostIPs.insert_one(entry)
 
-	hostIPs = [obj['hostIP'] for obj in mongo.db.hostIPs.find()]
-
-	return render_template('ping.html', hostIPs=hostIPs)
+    hostIPs = [obj['hostIP'] for obj in mongo.db.hostIPs.find()]
+    return render_template('ping.html', hostIPs=hostIPs, error=error)
 
 app.run(host='0.0.0.0', port=8000)
--- NetworkMonitor/util.py
+++ NetworkMonitor/util.py
@@ -3,7 +3,12 @@ import subprocess
 def ping(host):
 	"""Pings `host`, returns average rtt and jitter"""
 	cmd = 'ping {} -c 5 -i 0.2 -q'.format(host) # host could be nasty!!! careful
-	status = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+	try:
+		status = subprocess.run(cmd.split(), stdout=subprocess.PIPE, timeout=5)
+	except subprocess.TimeoutExpired:
+		raise ValueError('Host timed out')
+	if status.returncode != 0:
+		raise ValueError('Host not reachable')
 	output = status.stdout.decode('utf-8')
 	stat_index = output.find('=')+2
 	min_rtt, avg_rtt, max_rtt, mdev_rt = output[stat_index:].split('/')
